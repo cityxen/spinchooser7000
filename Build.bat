a19d8d2c4032f920f8c47e7fcbbd27f2e5f9d0d0
deadline-cxn <deadline@cityxen.net>
2023-07-31 12:58:14 -0400

    Spin Chooser 7000 SFGE post update

--- Build.bat
+++ Build.bat
@@ -3,7 +3,7 @@ echo Build Script: Building %1
 call genkickass-script.bat -t C64 -o prg_files -m true -s true -l "RETRO_DEV_LIB"
 call KickAss.bat spinchooser7k.asm
 
-ftp -s:ftp.u64
+#ftp -s:ftp.u64
 ftp -s:ftp.u2
 
 
